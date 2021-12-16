ef3f99b84194c6dce7e8e463c8f71ed3f9e05d0d
Kaviyashre Ragupathy <60558428+RPK2103@users.noreply.github.com>
2021-12-16 23:13:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function caclulate() {
     .then(data => {
       // console.log(data);
       const rate = data.rates[currency_two];
-
       rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
 
       amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
